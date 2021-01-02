a9f7f5acf221513dce27966a3ec6205b865ac9d3
kartheek1234 <74406192+kartheek1234@users.noreply.github.com>
2021-01-02 15:48:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var fixedRect, movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect =createSprite(200, 200, 30, 100);
+  movingRect= createSprite(200,200,100,30);
+  fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+  movingRect.debug=true;
+  fixedRect.debug=true;
+}
+
+function draw() {
+  background("black");  
+  movingRect.x= World.mouseX;
+  movingRect.y= World.mouseY;
+  if(movingRect.x-fixedRect.x<=movingRect.width/2+fixedRect.width/2
+    && fixedRect.x-movingRect.x<=fixedRect.width/2+movingRect.width/2
+    && movingRect.y-fixedRect.y<=movingRect.height/2+fixedRect.height/2
+    && fixedRect.y-movingRect.y<=fixedRect.height/2+movingRect.height/2){
+    fixedRect.shapeColor="red";
+  movingRect.shapeColor="red";
+  }
+  else{
+    fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+  }
+  drawSprites();
+}
